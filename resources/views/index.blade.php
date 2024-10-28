6008c5a474a8cc3fc4d2df4c48ed5fb1630adfc9
Bilal van Loon <vanloon_1983@hotmail.com>
2024-10-28 22:30:28 +0100

    Finished the simple tasks list manager

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,21 @@
 @section('title', 'The list of tasks')
 
 @section('content')
+    <nav class="mb-4">
+        <a class="link" href="{{ route('tasks.create') }}">Add Task</a>
+    </nav>
+
     @forelse ($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            <a @class(['line-through' => $task->completed]) href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
         </div>
     @empty
         <div>There are no tasks</div>
     @endforelse
+
+    @if ($tasks->count())
+        <nav class="mt-4">
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
